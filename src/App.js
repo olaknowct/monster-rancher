532e086675d24b69931850f82c44d41c59454e00
Christopher Olano <olanochristopher@gmail.com>
2021-01-16 21:32:26 +0800

    componentdidmount: emphasis and used case

--- src/App.js
+++ src/App.js
@@ -7,22 +7,39 @@ class App extends Component {
         super();
 
         this.state = {
-            string: "Hello Chruss",
+            monster: [
+                {
+                    name: "frankenstei",
+                    id: "aasc1",
+                },
+                {
+                    name: "alucard",
+                    id: "aa2sc1",
+                },
+                {
+                    name: "drankula",
+                    id: "aas3c1",
+                },
+                {
+                    name: "einstein",
+                    id: "a4asc1",
+                },
+            ],
         };
+    }
+
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((res) => res.json())
+            .then((users) => this.setState({ monster: users }));
     }
 
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>{this.state.string}</p>
-                    <button
-                        onClick={() => this.setState({ string: "amazing" })}
-                    >
-                        text change
-                    </button>
-                </header>
+                {this.state.monster.map((monster) => (
+                    <h1 key={monster.id}>{monster.name}</h1>
+                ))}
             </div>
         );
     }
